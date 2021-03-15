21cd0e311324f31c115f1cab482d35aaf7635931
jistol <pptwenty@gmail.com>
2021-03-15 20:14:59 +0900

    - formatter 수정

--- formatter/formatter.js
+++ formatter/formatter.js
@@ -1,2 +1,21 @@
 'use strict';
-alert('formatter!!!');
+(function() {
+  const doConvert = function() {
+    let newLine = document.getElementById('newLine');
+    let source = document.getElementById('source');
+    let destination = document.getElementById('destination');
+
+    console.log(`src : ${source.value}, newLine : ${newLine.value}`);
+
+    let src = source.value;
+    src = replaceSource(src, newLine.value, '<br/>');
+    destination.innerHTML = src;
+  };
+
+  const replaceSource = function(src, from, to) {
+    return src.replaceAll(from, to);
+  }
+
+  let btnConvert = document.getElementById('btnConvert');
+  btnConvert.addEventListener('click', doConvert)
+})();
