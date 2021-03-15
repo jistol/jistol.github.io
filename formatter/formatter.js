95613b6914c34e30a7c14d0ce5f65de98be0e412
jistol <pptwenty@gmail.com>
2021-03-15 21:32:19 +0900

    - highlight 추가

--- formatter/formatter.js
+++ formatter/formatter.js
@@ -2,16 +2,35 @@
 (function() {
   const doConvert = function() {
     let src = document.getElementById('source').value;
-    let targets = document.getElementsByName("replaceTarget");
-    for (let t of targets) {
-      src = replaceSource(src, t.value, t.getAttribute('to'));
-    }
+
+    src = src.replaceAll('<', '&lt;');
+    src = src.replaceAll('>', '&gt;');
+    src = replaceSource(src);
+    src = highlightSource(src);
+
     document.getElementById('destination').innerHTML = src;
   };
 
-  const replaceSource = function(src, from, to) {
-    return (from && from.length > 0) ? src.replaceAll(from, to) : src;
-  }
+  const replaceSource = function(src) {
+    let targets = document.getElementsByName("replaceTarget");
+    for (let t of targets) {
+      let from = t.value;
+      let to = t.getAttribute('to');
+      src = (from && from.length > 0) ? src.replaceAll(from, to) : src;
+    }
+    return src;
+  };
+
+  const highlightSource = function(src) {
+    let useHighlight = document.getElementById('highlight').value;
+    if (useHighlight) {
+      let regex = new RegExp(document.getElementById('highlightRegex').value);
+      let color = document.getElementById('highlightColor');
+      src = src.replace(regex, `<font color="${color}">\$&</font>`);
+    }
+
+    return src;
+  };
 
   let btnConvert = document.getElementById('btnConvert');
   btnConvert.addEventListener('click', doConvert)
