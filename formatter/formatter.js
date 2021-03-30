09c4ca04fbafe869e5c3c14204dc390c48f6a279
jistol <pptwenty@gmail.com>
2021-03-30 09:31:41 +0900

    - json 변환 기능 추가

--- formatter/formatter.js
+++ formatter/formatter.js
@@ -1,15 +1,22 @@
 'use strict';
 (function() {
-  const doConvert = function() {
+  const convertSource = function() {
     let src = document.getElementById('source').value;
 
     src = src.replaceAll('<', '&lt;');
     src = src.replaceAll('>', '&gt;');
     src = replaceSource(src);
     src = highlightSource(src);
+    return src;
+  }
 
-    document.getElementById('destination').innerHTML = src;
+  const doConvert = function() {
+    document.getElementById('destination').innerHTML = convertSource();
   };
+
+  const toJson = function() {
+    $('#destination').jsonView(convertSource());
+  }
 
   const replaceSource = function(src) {
     let targets = document.getElementsByName("replaceTarget");
@@ -34,4 +41,7 @@
 
   let btnConvert = document.getElementById('btnConvert');
   btnConvert.addEventListener('click', doConvert)
+
+  let btnToJson = document.getElementById('btnToJson');
+  btnToJson.addEventListener('click', toJson)
 })();
